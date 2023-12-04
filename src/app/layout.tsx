ce423bfd3d87bc8f0e77370c03eb462958c0d0f1
ansonngg <ngkawai9886@gmail.com>
2023-12-03 22:57:36 -0500

    Install vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.scss';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={montserrat.className}>{children}</body>
+            <body className={montserrat.className}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
